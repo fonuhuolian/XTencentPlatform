dc2b750e6f8ce8382f6fe1d75e81bfe93484618e
MyAndroidStore <2019880507@qq.com>
2019-10-10 09:17:26 +0800

    Update IWechaListener.java

--- xtencentplatform/src/main/java/com/fonuhuolian/xtencentplatform/IWechaListener.java
+++ xtencentplatform/src/main/java/com/fonuhuolian/xtencentplatform/IWechaListener.java
@@ -4,7 +4,12 @@ import com.fonuhuolian.xtencentplatform.bean.WechatUserInfo;
 
 public interface IWechaListener {
 
+    // 获取信息开始前
+    void onStart();
+
+    // 获取失败
     void onFail(String errMsg);
-    void onSuccess(WechatUserInfo loginResp);
-    void onToken(String msg);
+
+    // 获取成功
+    void onSuccess(WechatUserInfo info);
 }
